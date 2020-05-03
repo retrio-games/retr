e82c6aae6c01efbd37e0b887af17f3324df62516
btekl <brian.teklits@gmail.com>
2020-05-03 14:39:05 -0600

    Another try

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
 	user: 'postgres',
 	password: 'password'
 };*/
-const dbString = process.env.DATABASE_URL || "postgres://jbzadjlaaamuew:e52dd7d0d723b507ccc092da748804d2e30681640dc25c64408fed434ed22b6c@ec2-18-215-99-63.compute-1.amazonaws.com:5432/d7jc2eheg2ukl5"
+const dbString = "postgres://jbzadjlaaamuew:e52dd7d0d723b507ccc092da748804d2e30681640dc25c64408fed434ed22b6c@ec2-18-215-99-63.compute-1.amazonaws.com:5432/d7jc2eheg2ukl5"
 
 const dbConfig = dbString;
 
